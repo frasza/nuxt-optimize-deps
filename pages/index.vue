f32d61287ac2bf544d003bfe2a4951509d74e12f
frasza <zan.fraas@gmail.com>
2023-11-08 18:52:07 +0100

    Init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { EditorContent, useEditor } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+
+const editor = useEditor({
+  content: "<p>Content here</p>",
+  extensions: [StarterKit],
+});
+</script>
+
+<template>
+  <div v-if="editor">
+    <EditorContent :editor="editor" />
+  </div>
+</template>
